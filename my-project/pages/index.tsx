5e76b352b31291e8d82e27beeeefbf0506a0db9a
Reece Bailey <80913719+Rbailey8857@users.noreply.github.com>
2023-01-18 00:17:30 -0800

    Update index.tsx

--- my-project/pages/index.tsx
+++ my-project/pages/index.tsx
@@ -18,10 +18,10 @@ const Home: NextPage = () => {
         <div className='flex items-center justify-center h-screen'>
           <ul className='mb-40'>
             <li className='text-primary md:text-5xl lg:text-7xl xlg:text-7xl text-2xl font-pressStart text-center'>RBTechPro</li>
-            <li className='text-white lg:text-2xl xlg:text-2xl font-pressStart text-center mt-5 py-3 w-screen hover:bg-primary'><Link href="/about">About</Link></li>
-            <li className='text-white lg:text-2xl xlg:text-2xl font-pressStart text-center  py-3 w-screen hover:bg-primary'><Link href="/projects">Projects</Link></li>
-            <li className='text-white lg:text-2xl xlg:text-2xl font-pressStart text-center  py-3 w-screen hover:bg-primary'><Link href="/experience">Experience</Link></li>
-            <li className='text-white lg:text-2xl xlg:text-2xl font-pressStart text-center  py-3 hover:bg-primary'><Link href="/contact">Contact</Link></li>
+            <Link href="/about"><li className='text-white lg:text-2xl xlg:text-2xl font-pressStart text-center mt-5 py-3 w-screen hover:bg-primary'>About</li></Link>
+            <Link href="/projects"><li className='text-white lg:text-2xl xlg:text-2xl font-pressStart text-center  py-3 w-screen hover:bg-primary'>Projects</li></Link>
+            <Link href="/experience"><li className='text-white lg:text-2xl xlg:text-2xl font-pressStart text-center  py-3 w-screen hover:bg-primary'>Experience</li></Link>
+            <Link href="/contact"><li className='text-white lg:text-2xl xlg:text-2xl font-pressStart text-center  py-3 hover:bg-primary'>Contact</li></Link>
             <li className='text-secondary font-pressStart sm:invisible md:visible lg:visible xlg:visible text-center py-3'>Develop • Deliver • Dependable</li>
             <li className='text-secondary relative bottom-16 font-pressStart sm:visible md:invisible lg:invisible xlg:invisible text-center py-3'>Develop • Deliver Dependable</li>
           </ul>
