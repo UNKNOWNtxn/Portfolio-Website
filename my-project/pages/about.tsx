0621c81a5e16ef9e258e2799dab23e6b5d6e7925
Reece Bailey <80913719+Rbailey8857@users.noreply.github.com>
2023-01-17 21:43:18 -0800

    Marquee

--- my-project/pages/about.tsx
+++ my-project/pages/about.tsx
@@ -116,6 +116,86 @@ const About: NextPage = () => {
                     I am a hardworker, fast learner and experienced programmer currently studying computer science. My professional goal is to work with cutting-edge technologies and collaborate with a talented team of developers to create innovative software solutions. Javascript, C++ and Solidity are my most comfortable programming languages, but I am still proficient in a variety of UI frameworks such as TailwindCSS, Bootstrap and more. I have a strong understanding and passion for blockchain technologies including but not limited to; Ethereum, Hardhat and decentralized applications. On my off time I enjoying snowboarding and also playing video games like Overwatch, Call of Duty and Rocket League. Feel free to <Link className='underline underline-offset-1' href='/contact'>contact</Link> me to learn more about my experience, skills and projects, and to discuss potential opportunities.
                 </p>
             </div>
+
+            <div className="absolute bottom-0 flex overflow-x-hidden">
+                <div className="py-12 animate-marquee whitespace-nowrap">
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                </div>
+
+                <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                    <span className="text-6xl mx-4">🎉</span>
+                    <span className="text-6xl mx-4">🎂</span>
+                </div>
+            </div>
         </main>
   
         
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -34,6 +34,20 @@ module.exports = {
       'white': '#ffffff',
     },
     extend: {
+      animation: {
+        marquee: 'marquee 5s linear infinite',
+        marquee2: 'marquee2 5s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
       fontFamily: {
         pressStart: ['"Press Start 2P"', 'cursive'],
       }
